66c39f7f27f01221f51e8df2df3477560c51bc52
nghiphu <tnphvan@gmail.com>
2024-09-27 08:48:12 +0700

    Add project files.

---
+++ C500Hemis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C500Hemis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ C500Hemis/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<C500Hemis.Models.HemisContext>(options =>
+    options.UseSqlServer("Server=tcp:c500.database.windows.net,1433;Database=dbHemisC500;Trusted_Connection=True;Id=c500;Password=@Abc1234" ));
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
