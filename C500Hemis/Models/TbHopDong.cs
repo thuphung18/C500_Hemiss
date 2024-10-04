efbe11ec26289cdcd79e30be94b95aab0dbb9bdb
Nhóm 3 <htrewq1234@gmail.com>
2024-10-04 09:25:20 +0700

    Nhóm 3

--- C500Hemis/Models/TbHopDong.cs
+++ C500Hemis/Models/TbHopDong.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using C500Hemis.Models.DM;
+using Newtonsoft.Json.Serialization;
 
 namespace C500Hemis.Models;
 
@@ -9,26 +12,29 @@ public partial class TbHopDong
     public int IdHopDong { get; set; }
 
     public int? IdCanBo { get; set; }
-
+    [DisplayName(displayName: "Số hợp đồng")]
     public string? SoHopDong { get; set; }
 
     public int? IdLoaiHopDong { get; set; }
-
+    [DisplayName(displayName: "Số quyết định")]
     public string? SoQuyetDinh { get; set; }
-
+    [DisplayName(displayName: "Ngày quyết định")]
+    [Required(ErrorMessage = "Nhập đủ ngày tháng năm")]
     public DateOnly? NgayQuyetDinh { get; set; }
-
+    [Required(ErrorMessage = "Chỉ nhập ngày")]
+    [DisplayName(displayName: "Nhập đủ ngày tháng năm")]
     public DateOnly? CoGiaTriTu { get; set; }
-
+    [Required(ErrorMessage = "Chỉ nhập ngày")]
+    [DisplayName(displayName: "Nhập đủ ngày tháng năm")]
     public DateOnly? CoGiaTriDen { get; set; }
-
+    [DisplayName(displayName: "Tình trạng hợp đồng")]
     public int? IdTinhTrangHopDong { get; set; }
-
+    [DisplayName(displayName: "Làm việc toàn thời gian")]
     public bool? LamViecToanThoiGian { get; set; }
-
+    [DisplayName(displayName: "Cán bộ")]
     public virtual TbCanBo? IdCanBoNavigation { get; set; }
-
+    [DisplayName(displayName: "Loại hợp đồng")]
     public virtual DmLoaiHopDong? IdLoaiHopDongNavigation { get; set; }
-
+    [DisplayName(displayName: "Tình trạng hợp đồng")]
     public virtual DmTinhTrangHopDong? IdTinhTrangHopDongNavigation { get; set; }
 }
