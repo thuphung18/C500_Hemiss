207294bd0a62332fdde5bac68ebd06a0fdf99bde
Stanford Pines <ntsaz123@gmail.com>
2024-10-08 20:11:50 +0700

    Nhóm 8 B8D55

--- C500Hemis/Models/TbNganhDungTenGiangDay.cs
+++ C500Hemis/Models/TbNganhDungTenGiangDay.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using C500Hemis.Models.DM;
 
 namespace C500Hemis.Models;
 
 public partial class TbNganhDungTenGiangDay
 {
+
     public int IdNganhDungTenGiangDay { get; set; }
 
     public int? IdCanBo { get; set; }
 
     public int? IdNganhDaoTao { get; set; }
+    public string? TenCanBo { get; set; }
+    public string? TenNganhGiangDay { get; set; }
+
+    [DataType(DataType.Date)]
+    //NguyenDangPhuc 
+    //Nguyen Tan Son
+
+    public DateTime? NgayBatDau { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime? NgayKetThuc { get; set; }
 
     public double? TrongSo { get; set; }
 
