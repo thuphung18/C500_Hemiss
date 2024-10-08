0a6359bd212578c6d1fe2785750e01896bdb48ac
USHP <phuphudethuong@gmail.com>
2024-10-08 08:53:34 +0700

    Revert "Nhóm 7 B8D55"
    This reverts commit 9e9201b3d4c5c9fde978e186d3c2a69b5ba1c985.

--- C500Hemis/Models/TbLinhVucNghienCuuCuaCanBo.cs
+++ C500Hemis/Models/TbLinhVucNghienCuuCuaCanBo.cs
@@ -1,31 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using C500Hemis.Models.DM;
 
 namespace C500Hemis.Models;
 
 public partial class TbLinhVucNghienCuuCuaCanBo
 {
-    [DisplayName(displayName: "ID lĩnh vực nghiên cứu của cán bộ")]
     public int IdLinhVucNghienCuuCuaCanBo { get; set; }
 
-    [DisplayName(displayName: "ID Cán bộ")]
     public int? IdCanBo { get; set; }
 
-    [DisplayName(displayName: "ID lĩnh vực nghiên cứu")]
     public int? IdLinhVucNghienCuu { get; set; }
 
-    [DisplayName(displayName: "Là lĩnh vực nghiên cứu chuyên sâu")]
+    [DisplayName (displayName: "Lĩnh vực nghiên cứu chuyên sâu")]
     public bool? LaLinhVucNghienCuuChuyenSau { get; set; }
 
-    [DisplayName(displayName: "Số năm nghiên cứu")]
+    [DisplayName (displayName: "Số năm nghiên cứu")]
     public int? SoNamNghienCuu { get; set; }
 
-    [DisplayName(displayName: "Id cán bộ navigation")]
+    [DisplayName (displayName: "Mã số Id của cán bộ")]
     public virtual TbCanBo? IdCanBoNavigation { get; set; }
 
-    [DisplayName(displayName: "Id lĩnh vực nghiên cứu navigation")]
+    [DisplayName (displayName: "Mã số Id của lĩnh vực nghiên cứu")]
     public virtual DmLinhVucNghienCuu? IdLinhVucNghienCuuNavigation { get; set; }
 }
