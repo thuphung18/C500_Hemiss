afab33362e68846e31a0793d526458f126d5a2b7
Phạm Mạnh Đức <pduc31920@gmail.com>
2024-10-08 07:57:05 +0700

    nhóm 2 update

--- C500Hemis/Models/TbGiaoVienQpan.cs
+++ C500Hemis/Models/TbGiaoVienQpan.cs
@@ -1,25 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using C500Hemis.Models.DM;
 
 namespace C500Hemis.Models;
 
 public partial class TbGiaoVienQpan
 {
+    [DisplayName(displayName:"Id giáo viên")]
     public int IdGiaoVienQpan { get; set; }
-
+    [DisplayName(displayName: "Id cán bộ")]
     public int? IdCanBo { get; set; }
-
+    [DisplayName(displayName: "Năm bắt đầu biệt phái")]
     public DateOnly? NamBatDauBietPhai { get; set; }
-
+    [DisplayName(displayName: "Số năm biệt phái")]
     public DateOnly? SoNamBietPhai { get; set; }
-
+    [DisplayName(displayName: "Id loại giảng viên")]
     public int? IdLoaiGiangVienQp { get; set; }
-
+    [DisplayName(displayName: "Đào tạo gdqpan")]
     public string? DaoTaoGdqpan { get; set; }
-
+    [DisplayName(displayName: "Id quân hàm")]
     public int? IdQuanHam { get; set; }
-
+    [DisplayName(displayName: "Sở trường công tác")]
     public string? SoTruongCongTac { get; set; }
 
     public virtual TbCanBo? IdCanBoNavigation { get; set; }
