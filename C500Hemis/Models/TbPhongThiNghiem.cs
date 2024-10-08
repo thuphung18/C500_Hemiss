a679a72daf94e2084ab6c64d9d3b7b66ddced705
C500Hemis <hieuhayho2k5@gmail.com>
2024-10-08 09:34:39 +0700

    update

--- C500Hemis/Models/TbPhongThiNghiem.cs
+++ C500Hemis/Models/TbPhongThiNghiem.cs
@@ -1,26 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using C500Hemis.Models.DM;
+﻿using C500Hemis.Models.DM;
+using System.ComponentModel;
 
 namespace C500Hemis.Models;
 
 public partial class TbPhongThiNghiem
 {
+    [DisplayName("STT ")]
     public int IdPhongThiNghiem { get; set; }
+    [DisplayName("STT Công trình")]
 
     public int? IdCongTrinhCsvc { get; set; }
+    [DisplayName("STT Loại phòng thí nghiệm ")]
 
     public int? IdLoaiPhongThiNghiem { get; set; }
+    [DisplayName("STT Lĩnh vực ")]
 
     public int? IdLinhVuc { get; set; }
+    [DisplayName("Mức độ đáp ứng nhu cầu NCKH ")]
 
     public string? MucDoDapUngNhuCauNckh { get; set; }
+    [DisplayName("Năm đưa vào sử dụng ")]
 
     public string? NamDuaVaoSuDung { get; set; }
+    [DisplayName("Công trình cơ sở vật chất ")]
 
     public virtual TbCongTrinhCoSoVatChat? IdCongTrinhCsvcNavigation { get; set; }
+    [DisplayName("Lĩnh vực ")]
 
     public virtual DmLinhVucNghienCuu? IdLinhVucNavigation { get; set; }
+    [DisplayName("Loại phòng thí nghiệm ")]
 
     public virtual DmLoaiPhongThiNghiem? IdLoaiPhongThiNghiemNavigation { get; set; }
 }
