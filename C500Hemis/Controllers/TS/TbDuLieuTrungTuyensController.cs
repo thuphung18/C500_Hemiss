92d532f4ec53b608de6d50e5fb2a88734a2b9c06
USHP <phuphudethuong@gmail.com>
2024-10-11 08:43:24 +0700

    cập nhật lại

--- C500Hemis/Controllers/TS/TbDuLieuTrungTuyensController.cs
+++ C500Hemis/Controllers/TS/TbDuLieuTrungTuyensController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using C500Hemis.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using C500Hemis.Models;
 
 namespace C500Hemis.Controllers.TS
 {
