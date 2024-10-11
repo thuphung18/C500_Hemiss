8551a28548f600ce04c013fd213f13f5da0171d5
Phạm Mạnh Đức <pduc31920@gmail.com>
2024-10-11 09:41:15 +0700

    update

--- C500Hemis/Models/TbNgonNguGiangDay.cs
+++ C500Hemis/Models/TbNgonNguGiangDay.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using C500Hemis.Models.DM;
 
 namespace C500Hemis.Models;
 
 public partial class TbNgonNguGiangDay
 {
+    [DisplayName(displayName: "Ngôn Ngữ Giảng Dạy")]
     public int IdNgonNguGiangDay { get; set; }
-
+    [DisplayName(displayName: "Chương Trình Đào Tạo")]
     public int? IdChuongTrinhDaoTao { get; set; }
-
+    [DisplayName(displayName: "Ngôn Ngữ")]
     public int? IdNgonNgu { get; set; }
-
+    [DisplayName(displayName: "Trình Độ Ngôn Ngữ Đầu Vào")]
     public int? IdTrinhDoNgonNguDauVao { get; set; }
-
+    [DisplayName(displayName: "Chương Trình Đào Tạo")]
     public virtual TbChuongTrinhDaoTao? IdChuongTrinhDaoTaoNavigation { get; set; }
-
+    [DisplayName(displayName: "Ngôn Ngữ")]
     public virtual DmNgoaiNgu? IdNgonNguNavigation { get; set; }
-
+    [DisplayName(displayName: "Trình Độ Ngôn Ngữ Đầu Vào")]
     public virtual DmKhungNangLucNgoaiNgu? IdTrinhDoNgonNguDauVaoNavigation { get; set; }
 }
