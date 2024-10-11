d5aeccdd43360ae224803af6a5c1a2d9ed3d1fc0
nhl-11 <hoanglinh111105@gmail.com>
2024-10-11 08:41:45 +0700

    .

--- C500Hemis/Models/TbKhoanNopNganSach.cs
+++ C500Hemis/Models/TbKhoanNopNganSach.cs
@@ -5,13 +5,18 @@ namespace C500Hemis.Models;
 
 public partial class TbKhoanNopNganSach
 {
+    [Display(Name = "ID Khoản Nộp Ngân Sách")]
     public int IdKhoanNopNganSach { get; set; }
 
+    [Display(Name = "Mã Khoản Nộp")]
     public string? MaKhoanNop { get; set; }
 
+    [Display(Name = "Tên Khoản Nộp Ngân Sách")]
     public string? TenKhoanNopNganSach { get; set; }
 
+    [Display(Name = "Số Tiền")]
     public int? SoTien { get; set; }
 
+    [Display(Name = "Năm Tài Chính")]
     public string? NamTaiChinh { get; set; }
 }
