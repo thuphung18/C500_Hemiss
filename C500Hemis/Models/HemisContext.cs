a139636caa6b0dd10a5237dbe2687bc65c46bc83
Thanh Phong Cao <hong99152@gmail.com>
2024-10-13 15:50:03 +0700

    UPDATE SERVERNAME

--- C500Hemis/Models/HemisContext.cs
+++ C500Hemis/Models/HemisContext.cs
@@ -625,7 +625,8 @@ public partial class HemisContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
        // => optionsBuilder.UseSqlServer("Server=172.20.10.2\\SQLEXPRESS;Encrypt=false;Database=dbHemisC500;User ID=sa;Password=@Abc123");
-            => optionsBuilder.UseSqlServer("Server=tcp:c500.database.windows.net,1433;Encrypt=false;Database=dbHemisC500;User ID=c500;Password=@Abc1234");
+            //=> optionsBuilder.UseSqlServer("Server=tcp:c500.database.windows.net,1433;Encrypt=false;Database=dbHemisC500;User ID=c500;Password=@Abc1234");
+            => optionsBuilder.UseSqlServer("Server=tcp:ctphongtest.database.windows.net,1433;Encrypt=false;Database=dbHemisC500;User ID=zoo1123;Password=@dbHemisC5001");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
