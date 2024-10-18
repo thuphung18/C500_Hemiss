90ba60b15176f5e5fbe611903c4939c941e0aa01
Nhóm 3 <htrewq1234@gmail.com>
2024-10-18 08:00:08 +0700

    Revert "AT.026 B8D55"
    This reverts commit 023435912ba362b5c33c9090559b9891431f5ad0.

--- C500Hemis/Models/VPhongThiNghiem.cs
+++ C500Hemis/Models/VPhongThiNghiem.cs
@@ -1,23 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace C500Hemis.Models;
 
 public partial class VPhongThiNghiem
 {
-    [DisplayName("Mã số công trình")]
-    
     public string? MaCongTrinh { get; set; }
-    [DisplayName("Loại phòng thí nghiệm")]
 
     public string? LoaiPhongThiNghiem { get; set; }
-    [DisplayName("Lĩnh vực nghiên cứu")]
+
     public string? LinhVucNghienCuu { get; set; }
-    [DisplayName("Mức độ đáp ứng nhu cầu NCKH")]
 
     public string? MucDoDapUngNhuCauNckh { get; set; }
-    [DisplayName("Năm đưa vào sử dụng")]
+
     public string? NamDuaVaoSuDung { get; set; }
 }
