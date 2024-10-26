2491df06cb07235a191df5526b43bdfca99cdf6a
tengicungduocnhe <tranthinhung15072004@gmail.com>
2024-10-26 12:04:18 +0700

    TRẦN THỊ NHUNG - CHUYỂN GIAO CÔNG NGHỆ VÀ ĐÀO TẠO

--- C500Hemis/Controllers/KHCN/ChuyenGiaoCongNgheVaDaoTaoController.cs
+++ C500Hemis/Controllers/KHCN/ChuyenGiaoCongNgheVaDaoTaoController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using C500Hemis.Models;
+using Microsoft.CodeAnalysis.Options;
 
 namespace C500Hemis.Controllers.KHCN
 {
@@ -19,10 +20,36 @@ namespace C500Hemis.Controllers.KHCN
         }
 
         // GET: ChuyenGiaoCongNgheVaDaoTao
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index()
+        //{
+        //    var hemisContext = _context.TbChuyenGiaoCongNgheVaDaoTaos.Include(t => t.IdHinhThucChuyenGiaoCongNgheNavigation).Include(t => t.IdLinhVucNghienCuuNavigation).Include(t => t.IdNganhKinhTeNavigation).Include(t => t.IdNhiemVuKhcnNavigation).Include(t => t.IdTrangThaiHopDongNavigation);
+        //    return View(await hemisContext.ToListAsync());
+        //}
+        public IActionResult Index(string Id, string SapXep)
         {
-            var hemisContext = _context.TbChuyenGiaoCongNgheVaDaoTaos.Include(t => t.IdHinhThucChuyenGiaoCongNgheNavigation).Include(t => t.IdLinhVucNghienCuuNavigation).Include(t => t.IdNganhKinhTeNavigation).Include(t => t.IdNhiemVuKhcnNavigation).Include(t => t.IdTrangThaiHopDongNavigation);
-            return View(await hemisContext.ToListAsync());
+            ViewBag.IdChuyenGiaoCongNgheVaDaoTao = Id;
+            HemisContext db = new HemisContext();
+            var kq = db.TbChuyenGiaoCongNgheVaDaoTaos.ToList(); // lấy danh sách 
+            var danhSach = db.TbChuyenGiaoCongNgheVaDaoTaos
+                .Include(item => item.IdHinhThucChuyenGiaoCongNgheNavigation)
+                .Include(item => item.IdLinhVucNghienCuuNavigation)
+                .Include(item => item.IdNganhKinhTeNavigation)
+                .Include(item => item.IdNhiemVuKhcnNavigation)
+                .Include(item => item.IdTrangThaiHopDongNavigation)
+                .Where(item => string.IsNullOrEmpty(Id) || item.IdChuyenGiaoCongNgheVaDaoTao.ToString() == Id) //  tìm kiếm theo IdChuyeenr giao công nghệ
+                .ToList();
+
+            var sapXepDanhSach = danhSach; // sắp xếp
+
+            if (SapXep == "SapXep")
+            {
+                sapXepDanhSach = danhSach.OrderBy(x => x.TongChiPhiThucHien).ToList();// sắp xếp theo tổng chi phí thực hiện
+            }
+
+            ViewBag.KqTimKiem = danhSach;
+            ViewBag.KqSapXep = sapXepDanhSach;
+
+            return View(sapXepDanhSach);
         }
 
         // GET: ChuyenGiaoCongNgheVaDaoTao/Details/5
@@ -51,11 +78,13 @@ namespace C500Hemis.Controllers.KHCN
         // GET: ChuyenGiaoCongNgheVaDaoTao/Create
         public IActionResult Create()
         {
-            ViewData["IdHinhThucChuyenGiaoCongNghe"] = new SelectList(_context.DmHinhThucChuyenGiaoCongNghes, "IdHinhThucChuyenGiaoCongNghe", "IdHinhThucChuyenGiaoCongNghe");
-            ViewData["IdLinhVucNghienCuu"] = new SelectList(_context.DmLinhVucNghienCuus, "IdLinhVucNghienCuu", "IdLinhVucNghienCuu");
-            ViewData["IdNganhKinhTe"] = new SelectList(_context.DmNganhKinhTes, "IdNganhKinhTe", "IdNganhKinhTe");
-            ViewData["IdNhiemVuKhcn"] = new SelectList(_context.TbNhiemVuKhcns, "IdNhiemVuKhcn", "IdNhiemVuKhcn");
-            ViewData["IdTrangThaiHopDong"] = new SelectList(_context.DmTrangThaiHopDongs, "IdTrangThaiHopDong", "IdTrangThaiHopDong");
+           // _context.TbCanBos.Include(h => h.IdNguoiNavigation), "IdCanBo", "IdNguoiNavigation.name", tbThongTinHocTapNghienCuuSinh.IdNguoiHuongDanChinh
+            ViewData["IdHinhThucChuyenGiaoCongNghe"] = new SelectList(_context.DmHinhThucChuyenGiaoCongNghes, "IdHinhThucChuyenGiaoCongNghe", "HinhThucChuyenGiaoCongNghe");
+            ViewData["IdLinhVucNghienCuu"] = new SelectList(_context.DmLinhVucNghienCuus, "IdLinhVucNghienCuu", "LinhVucNghienCuu");
+            ViewData["IdNganhKinhTe"] = new SelectList(_context.DmNganhKinhTes, "IdNganhKinhTe", "NganhKinhTe");
+            ViewData["IdNhiemVuKhcn"] = new SelectList(_context.TbNhiemVuKhcns, "IdNhiemVuKhcn", "TenNhiemVu");
+            ViewData["IdTrangThaiHopDong"] = new SelectList(_context.DmTrangThaiHopDongs, "IdTrangThaiHopDong", "TrangThaiHopDong");
+          
             return View();
         }
 
@@ -66,19 +95,45 @@ namespace C500Hemis.Controllers.KHCN
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("IdChuyenGiaoCongNgheVaDaoTao,IdNhiemVuKhcn,MaSoHopDong,Ten,TongChiPhiThucHien,TongThoiGianThucHien,IdHinhThucChuyenGiaoCongNghe,PhuongThucChuyenGiaoCongNghe,ChuSoHuu,DonViChuTri,DonViPhoiHop,DonViNhanChuyenGiao,TomTat,TenDuAn,GiaTriHopDong,IdNganhKinhTe,IdTrangThaiHopDong,SoNguoiDuocDaoTaoChuyenGiaoCn,IdLinhVucNghienCuu")] TbChuyenGiaoCongNgheVaDaoTao tbChuyenGiaoCongNgheVaDaoTao)
         {
+           
+
             if (ModelState.IsValid)
             {
-                _context.Add(tbChuyenGiaoCongNgheVaDaoTao);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    _context.Add(tbChuyenGiaoCongNgheVaDaoTao);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException ex)
+                {
+                    // Handle specific database update errors
+                    if (ex.InnerException != null && ex.InnerException.Message.Contains("PRIMARY KEY"))
+                    {
+                        ModelState.AddModelError("IdChuyenGiaoCongNgheVaDaoTao", "Mã chuyển giao công nghệ đã tồn tại.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Có lỗi xảy ra khi lưu dữ liệu. Vui lòng thử lại.");
+                    }
+                }
+                catch (OverflowException)
+                {
+                    ModelState.AddModelError(string.Empty, "Giá trị đã nhập vượt quá giới hạn cho phép. Vui lòng nhập giá trị nhỏ hơn.");
+                }
             }
-            ViewData["IdHinhThucChuyenGiaoCongNghe"] = new SelectList(_context.DmHinhThucChuyenGiaoCongNghes, "IdHinhThucChuyenGiaoCongNghe", "IdHinhThucChuyenGiaoCongNghe", tbChuyenGiaoCongNgheVaDaoTao.IdHinhThucChuyenGiaoCongNghe);
-            ViewData["IdLinhVucNghienCuu"] = new SelectList(_context.DmLinhVucNghienCuus, "IdLinhVucNghienCuu", "IdLinhVucNghienCuu", tbChuyenGiaoCongNgheVaDaoTao.IdLinhVucNghienCuu);
-            ViewData["IdNganhKinhTe"] = new SelectList(_context.DmNganhKinhTes, "IdNganhKinhTe", "IdNganhKinhTe", tbChuyenGiaoCongNgheVaDaoTao.IdNganhKinhTe);
-            ViewData["IdNhiemVuKhcn"] = new SelectList(_context.TbNhiemVuKhcns, "IdNhiemVuKhcn", "IdNhiemVuKhcn", tbChuyenGiaoCongNgheVaDaoTao.IdNhiemVuKhcn);
-            ViewData["IdTrangThaiHopDong"] = new SelectList(_context.DmTrangThaiHopDongs, "IdTrangThaiHopDong", "IdTrangThaiHopDong", tbChuyenGiaoCongNgheVaDaoTao.IdTrangThaiHopDong);
+
+            // Populate dropdowns for the view
+            ViewData["IdHinhThucChuyenGiaoCongNghe"] = new SelectList(_context.DmHinhThucChuyenGiaoCongNghes, "IdHinhThucChuyenGiaoCongNghe", "HinhThucChuyenGiaoCongNghe", tbChuyenGiaoCongNgheVaDaoTao.IdHinhThucChuyenGiaoCongNghe);
+            ViewData["IdLinhVucNghienCuu"] = new SelectList(_context.DmLinhVucNghienCuus, "IdLinhVucNghienCuu", "LinhVucNghienCuu", tbChuyenGiaoCongNgheVaDaoTao.IdLinhVucNghienCuu);
+            ViewData["IdNganhKinhTe"] = new SelectList(_context.DmNganhKinhTes, "IdNganhKinhTe", "NganhKinhTe", tbChuyenGiaoCongNgheVaDaoTao.IdNganhKinhTe);
+            ViewData["IdNhiemVuKhcn"] = new SelectList(_context.TbNhiemVuKhcns, "IdNhiemVuKhcn", "TenNhiemVu", tbChuyenGiaoCongNgheVaDaoTao.IdNhiemVuKhcn);
+            ViewData["IdTrangThaiHopDong"] = new SelectList(_context.DmTrangThaiHopDongs, "IdTrangThaiHopDong", "TrangThaiHopDong", tbChuyenGiaoCongNgheVaDaoTao.IdTrangThaiHopDong);
+
             return View(tbChuyenGiaoCongNgheVaDaoTao);
         }
+
+        //   <option value = "0" > @ViewBag.VuiLongChon </ option > 
 
         // GET: ChuyenGiaoCongNgheVaDaoTao/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -93,11 +148,11 @@ namespace C500Hemis.Controllers.KHCN
             {
                 return NotFound();
             }
-            ViewData["IdHinhThucChuyenGiaoCongNghe"] = new SelectList(_context.DmHinhThucChuyenGiaoCongNghes, "IdHinhThucChuyenGiaoCongNghe", "IdHinhThucChuyenGiaoCongNghe", tbChuyenGiaoCongNgheVaDaoTao.IdHinhThucChuyenGiaoCongNghe);
-            ViewData["IdLinhVucNghienCuu"] = new SelectList(_context.DmLinhVucNghienCuus, "IdLinhVucNghienCuu", "IdLinhVucNghienCuu", tbChuyenGiaoCongNgheVaDaoTao.IdLinhVucNghienCuu);
-            ViewData["IdNganhKinhTe"] = new SelectList(_context.DmNganhKinhTes, "IdNganhKinhTe", "IdNganhKinhTe", tbChuyenGiaoCongNgheVaDaoTao.IdNganhKinhTe);
-            ViewData["IdNhiemVuKhcn"] = new SelectList(_context.TbNhiemVuKhcns, "IdNhiemVuKhcn", "IdNhiemVuKhcn", tbChuyenGiaoCongNgheVaDaoTao.IdNhiemVuKhcn);
-            ViewData["IdTrangThaiHopDong"] = new SelectList(_context.DmTrangThaiHopDongs, "IdTrangThaiHopDong", "IdTrangThaiHopDong", tbChuyenGiaoCongNgheVaDaoTao.IdTrangThaiHopDong);
+            ViewData["IdHinhThucChuyenGiaoCongNghe"] = new SelectList(_context.DmHinhThucChuyenGiaoCongNghes, "IdHinhThucChuyenGiaoCongNghe", "HinhThucChuyenGiaoCongNghe", tbChuyenGiaoCongNgheVaDaoTao.IdHinhThucChuyenGiaoCongNghe);
+            ViewData["IdLinhVucNghienCuu"] = new SelectList(_context.DmLinhVucNghienCuus, "IdLinhVucNghienCuu", "LinhVucNghienCuu", tbChuyenGiaoCongNgheVaDaoTao.IdLinhVucNghienCuu);
+            ViewData["IdNganhKinhTe"] = new SelectList(_context.DmNganhKinhTes, "IdNganhKinhTe", "NganhKinhTe", tbChuyenGiaoCongNgheVaDaoTao.IdNganhKinhTe);
+            ViewData["IdNhiemVuKhcn"] = new SelectList(_context.TbNhiemVuKhcns, "IdNhiemVuKhcn", "TenNhiemVu", tbChuyenGiaoCongNgheVaDaoTao.IdNhiemVuKhcn);
+            ViewData["IdTrangThaiHopDong"] = new SelectList(_context.DmTrangThaiHopDongs, "IdTrangThaiHopDong", "TrangThaiHopDong", tbChuyenGiaoCongNgheVaDaoTao.IdTrangThaiHopDong);
             return View(tbChuyenGiaoCongNgheVaDaoTao);
         }
 
@@ -112,41 +167,45 @@ namespace C500Hemis.Controllers.KHCN
             {
                 return NotFound();
             }
-
+            // kiểm tra xem dữ liệu có tồn tại không
             if (ModelState.IsValid)
             {
                 try
                 {
+                    // update cái dữ liệu mới
                     _context.Update(tbChuyenGiaoCongNgheVaDaoTao);
+                    // lưu thay đổi
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
+                    // xem có tồn tại cái id này không, nếu k thi 404
                     if (!TbChuyenGiaoCongNgheVaDaoTaoExists(tbChuyenGiaoCongNgheVaDaoTao.IdChuyenGiaoCongNgheVaDaoTao))
                     {
                         return NotFound();
                     }
                     else
-                    {
+                    {// ném looix
                         throw;
                     }
                 }
+                //quay lại trang index
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["IdHinhThucChuyenGiaoCongNghe"] = new SelectList(_context.DmHinhThucChuyenGiaoCongNghes, "IdHinhThucChuyenGiaoCongNghe", "IdHinhThucChuyenGiaoCongNghe", tbChuyenGiaoCongNgheVaDaoTao.IdHinhThucChuyenGiaoCongNghe);
-            ViewData["IdLinhVucNghienCuu"] = new SelectList(_context.DmLinhVucNghienCuus, "IdLinhVucNghienCuu", "IdLinhVucNghienCuu", tbChuyenGiaoCongNgheVaDaoTao.IdLinhVucNghienCuu);
-            ViewData["IdNganhKinhTe"] = new SelectList(_context.DmNganhKinhTes, "IdNganhKinhTe", "IdNganhKinhTe", tbChuyenGiaoCongNgheVaDaoTao.IdNganhKinhTe);
-            ViewData["IdNhiemVuKhcn"] = new SelectList(_context.TbNhiemVuKhcns, "IdNhiemVuKhcn", "IdNhiemVuKhcn", tbChuyenGiaoCongNgheVaDaoTao.IdNhiemVuKhcn);
-            ViewData["IdTrangThaiHopDong"] = new SelectList(_context.DmTrangThaiHopDongs, "IdTrangThaiHopDong", "IdTrangThaiHopDong", tbChuyenGiaoCongNgheVaDaoTao.IdTrangThaiHopDong);
+            ViewData["IdHinhThucChuyenGiaoCongNghe"] = new SelectList(_context.DmHinhThucChuyenGiaoCongNghes, "IdHinhThucChuyenGiaoCongNghe", "HinhThucChuyenGiaoCongNghe", tbChuyenGiaoCongNgheVaDaoTao.IdHinhThucChuyenGiaoCongNghe);
+            ViewData["IdLinhVucNghienCuu"] = new SelectList(_context.DmLinhVucNghienCuus, "IdLinhVucNghienCuu", "LinhVucNghienCuu", tbChuyenGiaoCongNgheVaDaoTao.IdLinhVucNghienCuu);
+            ViewData["IdNganhKinhTe"] = new SelectList(_context.DmNganhKinhTes, "IdNganhKinhTe", "NganhKinhTe", tbChuyenGiaoCongNgheVaDaoTao.IdNganhKinhTe);
+            ViewData["IdNhiemVuKhcn"] = new SelectList(_context.TbNhiemVuKhcns, "IdNhiemVuKhcn", "TenNhiemVu", tbChuyenGiaoCongNgheVaDaoTao.IdNhiemVuKhcn);
+            ViewData["IdTrangThaiHopDong"] = new SelectList(_context.DmTrangThaiHopDongs, "IdTrangThaiHopDong", "TrangThaiHopDong", tbChuyenGiaoCongNgheVaDaoTao.IdTrangThaiHopDong);
             return View(tbChuyenGiaoCongNgheVaDaoTao);
         }
 
         // GET: ChuyenGiaoCongNgheVaDaoTao/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
+            if (id == null)// ktra xem id có null khôg
             {
-                return NotFound();
+                return NotFound();// nếu có thì 400
             }
 
             var tbChuyenGiaoCongNgheVaDaoTao = await _context.TbChuyenGiaoCongNgheVaDaoTaos
@@ -155,7 +214,7 @@ namespace C500Hemis.Controllers.KHCN
                 .Include(t => t.IdNganhKinhTeNavigation)
                 .Include(t => t.IdNhiemVuKhcnNavigation)
                 .Include(t => t.IdTrangThaiHopDongNavigation)
-                .FirstOrDefaultAsync(m => m.IdChuyenGiaoCongNgheVaDaoTao == id);
+                .FirstOrDefaultAsync(m => m.IdChuyenGiaoCongNgheVaDaoTao == id); // tìm kiếm giá trị ban đầu
             if (tbChuyenGiaoCongNgheVaDaoTao == null)
             {
                 return NotFound();
@@ -177,6 +236,36 @@ namespace C500Hemis.Controllers.KHCN
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteSelected([FromBody] List<int> selectedIds)
+        {
+            // kiểm tra những id được chọn
+            if (selectedIds == null || selectedIds.Count == 0)
+                
+            {
+                // nếu không thì báo như của message
+                return BadRequest(new { message = "Không có mục nào được chọn." });
+            }
+            // lấy danh sách mà những id dc chọn
+            var itemsToDelete = _context.TbChuyenGiaoCongNgheVaDaoTaos
+                .Where(x => selectedIds.Contains(x.IdChuyenGiaoCongNgheVaDaoTao))
+                .ToList();
+
+            if (itemsToDelete.Any())
+            {
+                // remove nó
+                _context.TbChuyenGiaoCongNgheVaDaoTaos.RemoveRange(itemsToDelete);
+                _context.SaveChanges();
+               // alert báo thành công
+                return Ok(new { message = "Xóa thành công." });
+            }
+            else
+            {
+                return NotFound(new { message = "Không tìm thấy các mục đã chọn." });
+            }
         }
 
         private bool TbChuyenGiaoCongNgheVaDaoTaoExists(int id)
