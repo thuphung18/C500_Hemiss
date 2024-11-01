616c0fb07fcf052e3072729f8e122e077913c0b7
luuducanhdunglqd2005@gmail.com <luuducanhdunglqd2005@gmail.com>
2024-11-01 09:20:35 +0700

    AT08.B8D55 commit

--- C500Hemis/Models/TbKhoanNopNganSach.cs
+++ C500Hemis/Models/TbKhoanNopNganSach.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace C500Hemis.Models;
+//commit
 
 public partial class TbKhoanNopNganSach
 {
